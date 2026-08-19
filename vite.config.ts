adb3e71247a75d31db997189d071b37386fbb1e1
prernachhajer <work.chhajer@gmail.com>
2026-08-19 10:41:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/prernachhajer/",
+ base: process.env.GITHUB_ACTIONS ? "/prernachhajer/" : "/",
 
   plugins: [
     react(),
