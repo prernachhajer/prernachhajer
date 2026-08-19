6e40b24224d646ab1fe8023d6a87fcb8838e9111
prernachhajer <work.chhajer@gmail.com>
2026-08-19 08:52:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+// https://vitejs.dev/config/
+export default defineConfig({
   base: "/prernachhajer/",
+
+  plugins: [
+    react(),
+  ],
 
   server: {
     host: "::",
@@ -20,32 +24,9 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
-  plugins: [
-    react(),
-
-    mode === "development" && componentTagger(),
-
-    {
-      name: "copy-index-to-404",
-
-      closeBundle() {
-        const fs = require("fs");
-        const path = require("path");
-
-        const distPath = path.resolve(__dirname, "dist");
-        const indexPath = path.join(distPath, "index.html");
-        const notFoundPath = path.join(distPath, "404.html");
-
-        fs.copyFileSync(indexPath, notFoundPath);
-
-        console.log("Created dist/404.html");
-      },
-    },
-  ].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
